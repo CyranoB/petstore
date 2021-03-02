9057157166ecffe831600ffab4db9407bddf840f
Eddie Pick <eddie@pick.fr>
2021-03-01 21:23:44 -0500

    Build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,3 +6,14 @@
 
   # Build Java App
   mvn -DskipTests=true package --no-transfer-progress 
+
+  # Push container to ECR in shared services
+  REPOSITORY_URI="615961246879.dkr.ecr.us-east-1.amazonaws.com/warner/looney/roadrunner"
+  COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
+  IMAGE_TAG=${COMMIT_HASH:=latest}
+
+  $(aws ecr get-login --region ca-central-1 --no-include-email)
+  docker build -t $REPOSITORY_URI:latest .
+  docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
+  docker push $REPOSITORY_URI:latest
+  docker push $REPOSITORY_URI:$IMAGE_TAG
