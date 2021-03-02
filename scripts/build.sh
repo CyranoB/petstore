3d5a193c1ec5ed1704095420aa36a3407fe21e67
Eddie Pick <eddie@pick.fr>
2021-03-01 22:06:25 -0500

    Change region in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@
   COMMIT_HASH=$(echo $CODEBUILD_RESOLVED_SOURCE_VERSION | cut -c 1-7)
   IMAGE_TAG=${COMMIT_HASH:=latest}
 
-  $(aws ecr get-login --region ca-central-1 --no-include-email)
+  $(aws ecr get-login --region us-east-1 --no-include-email)
   docker build -t $REPOSITORY_URI:latest .
   docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
   docker push $REPOSITORY_URI:latest
