92cdf7acb27fb76923f40f22f94fae719e7348d1
Eddie Pick <eddie@pick.fr>
2021-03-03 19:07:38 -0500

    Build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -u
 set -o pipefail
 
 wget -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.6.0.2311.zip
-unzip -qq -f -o sonar-scanner-cli-4.6.0.2311.zip
+unzip -qq sonar-scanner-cli-4.6.0.2311.zip
 SCANNER="sonar-scanner-4.6.0.2311/lib/sonar-scanner-cli-4.6.0.2311.jar"
 pip3 -q install truffleHog
 
